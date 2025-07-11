6ddb171b198799f7dca108ae35de6464755af994
Chan <yc01847@otter31.eps.surrey.ac.uk>
2025-07-11 14:19:37 +0100

    full config trial 1

--- train.sh
+++ train.sh
@@ -8,18 +8,18 @@ python train.py \
   --name mono2binaural \
   --model audioVisual \
   --checkpoints_dir /scratch/yc01847/2.5D-Visual-Sound/checkpoints \
-  --save_epoch_freq 10 \
-  --save_latest_freq 5 \
-  --display_freq 2 \
+  --save_epoch_freq 50 \
+  --save_latest_freq 100 \
+  --display_freq 10 \
   --batchSize 64 \
   --learning_rate_decrease_itr 10 \
-  --niter 10 \
+  --niter 1000 \
   --lr_visual 0.0001 \
   --lr_audio 0.001 \
-  --nThreads 0 \
+  --nThreads 24 \
   --gpu_ids 0 \
   --validation_on \
-  --validation_freq 5 \
-  --validation_batches 2 \
+  --validation_freq 100 \
+  --validation_batches 50 \
 
   |& tee -a mono2binaural.log
