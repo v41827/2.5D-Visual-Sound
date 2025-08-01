a1c5ea6c60a78cfda685df5846672237fe02f95f
Chan <yc01847@otter31.eps.surrey.ac.uk>
2025-08-01 16:25:50 +0100

    finish AVT trainings

--- train.sh
+++ train.sh
@@ -4,11 +4,11 @@ conda activate visual-sound
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 python train.py \
-  --hdf5FolderPath /scratch/yc01847/FAIR-Play/splits/split1 \
+  --hdf5FolderPath /scratch/yc01847/FAIR-Play/splits/split8 \
   --text_folder_name sampled_output_step10 \
   --name mono2binaural \
   --model audioVisual \
-  --checkpoints_dir /scratch/yc01847/2.5D-Visual-Sound/checkpoints/AVT_text_frozen/split1 \
+  --checkpoints_dir /scratch/yc01847/2.5D-Visual-Sound/checkpoints/AVT_text_frozen/split8 \
   --save_epoch_freq 50 \
   --save_latest_freq 100 \
   --display_freq 10 \
@@ -17,9 +17,48 @@ python train.py \
   --niter 500 \
   --lr_visual 0.0001 \
   --lr_audio 0.001 \
-  --nThreads 1 \
+  --nThreads 4 \
   --gpu_ids 0 \
   --validation_on \
   --validation_freq 100 \
   --validation_batches 50 |& tee -a mono2binaural.log
 
+python train.py \
+  --hdf5FolderPath /scratch/yc01847/FAIR-Play/splits/split9 \
+  --text_folder_name sampled_output_step10 \
+  --name mono2binaural \
+  --model audioVisual \
+  --checkpoints_dir /scratch/yc01847/2.5D-Visual-Sound/checkpoints/AVT_text_frozen/split9 \
+  --save_epoch_freq 50 \
+  --save_latest_freq 100 \
+  --display_freq 10 \
+  --batchSize 64 \
+  --learning_rate_decrease_itr 10 \
+  --niter 500 \
+  --lr_visual 0.0001 \
+  --lr_audio 0.001 \
+  --nThreads 4 \
+  --gpu_ids 0 \
+  --validation_on \
+  --validation_freq 100 \
+  --validation_batches 50 |& tee -a mono2binaural.log
+
+  python train.py \
+  --hdf5FolderPath /scratch/yc01847/FAIR-Play/splits/split10 \
+  --text_folder_name sampled_output_step10 \
+  --name mono2binaural \
+  --model audioVisual \
+  --checkpoints_dir /scratch/yc01847/2.5D-Visual-Sound/checkpoints/AVT_text_frozen/split10 \
+  --save_epoch_freq 50 \
+  --save_latest_freq 100 \
+  --display_freq 10 \
+  --batchSize 64 \
+  --learning_rate_decrease_itr 10 \
+  --niter 500 \
+  --lr_visual 0.0001 \
+  --lr_audio 0.001 \
+  --nThreads 4 \
+  --gpu_ids 0 \
+  --validation_on \
+  --validation_freq 100 \
+  --validation_batches 50 |& tee -a mono2binaural.log
