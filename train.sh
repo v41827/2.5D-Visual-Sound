ef0501e06fb9a363fde1ade54e469dae70aff495
Chan <yc01847@otter31.eps.surrey.ac.uk>
2025-08-18 20:12:13 +0100

    train split 1

--- train.sh
+++ train.sh
@@ -4,10 +4,10 @@ conda activate visual-sound
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 python train.py \
-  --hdf5FolderPath /scratch/yc01847/FAIR-Play/splits/split9 \
+  --hdf5FolderPath /scratch/yc01847/FAIR-Play/splits/split1 \
   --name mono2binaural \
   --model audioVisual \
-  --checkpoints_dir /scratch/yc01847/2.5D-Visual-Sound/checkpoints/split9 \
+  --checkpoints_dir /scratch/yc01847/2.5D-Visual-Sound/checkpoints/split1 \
   --save_epoch_freq 50 \
   --save_latest_freq 100 \
   --display_freq 10 \
@@ -16,7 +16,7 @@ python train.py \
   --niter 500 \
   --lr_visual 0.0001 \
   --lr_audio 0.001 \
-  --nThreads 1 \
+  --nThreads 4 \
   --gpu_ids 0 \
   --validation_on \
   --validation_freq 100 \
