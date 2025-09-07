becf00a74f1f00be84b68cde98086a1158b7012b
Chan <yc01847@localhost.surrey.ac.uk>
2025-09-07 01:50:59 +0100

    train split1 750 epcs

--- train.sh
+++ train.sh
@@ -3,20 +3,20 @@
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 python3 train.py \
-  --hdf5FolderPath /scratch/yc01847/FAIR-Play/splits/split7 \
+  --hdf5FolderPath /scratch/yc01847/FAIR-Play/splits/split1 \
   --text_folder_name sampled_output_step10 \
   --name mono2binaural \
   --model audioVisual \
-  --checkpoints_dir /scratch/yc01847/2.5D-Visual-Sound/checkpoints/AVT_text_frozen/split7 \
+  --checkpoints_dir /scratch/yc01847/2.5D-Visual-Sound/checkpoints/AVT_text_frozen/split1_750epc \
   --save_epoch_freq 50 \
   --save_latest_freq 100 \
   --display_freq 10 \
   --batchSize 64 \
   --learning_rate_decrease_itr 10 \
-  --niter 500 \
+  --niter 750 \
   --lr_visual 0.0001 \
   --lr_audio 0.001 \
-  --nThreads 4 \
+  --nThreads 6 \
   --gpu_ids 0 \
   --validation_on \
   --validation_freq 100 \
